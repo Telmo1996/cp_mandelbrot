5c283510390a2eba969a5935767af9ad37b85b9b
Telmo Fernández Corujo <telmo.fcorujo@udc.es>
2021-05-04 13:34:39 +0200

    separar filas

--- mandel.c
+++ mandel.c
@@ -24,78 +24,101 @@ with z_0 = 0 does not tend to infinity.*/
 
 typedef struct complextype
 {
-  float real, imag;
+	float real, imag;
 } Compl;
 
 static inline double get_seconds(struct timeval t_ini, struct timeval t_end)
 {
-  return (t_end.tv_usec - t_ini.tv_usec) / 1E6 +
+	return (t_end.tv_usec - t_ini.tv_usec) / 1E6 +
          (t_end.tv_sec - t_ini.tv_sec);
 }
 
 int main ( )
 {
+	MPI_Init(&argc, &argv);
 
-  /* Mandelbrot variables */
-  int i, j, k;
-  Compl   z, c;
-  float   lengthsq, temp;
-  int *vres, *res[Y_RESN];
+	/* Mandelbrot variables */
+	int i, j, k;
+	Compl   z, c;
+	float   lengthsq, temp;
+	int *vres, *res[Y_RESN];
+	int numProcs, rank;
+	int filasPorProceso;
 
-  /* Timestamp variables */
-  struct timeval  ti, tf;
+	MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  /* Allocate result matrix of Y_RESN x X_RESN */
-  vres = (int *) malloc(Y_RESN * X_RESN * sizeof(int));
-  if (!vres)
-  {
-    fprintf(stderr, "Error allocating memory\n");
-    return 1;
-  }
-  for (i=0; i<Y_RESN; i++)
-    res[i] = vres + i*X_RESN;
+	filasPorProceso = (int)Y_RESN/numProcs;
 
-  /* Start measuring time */
-  gettimeofday(&ti, NULL);
+	/* Timestamp variables */
+	struct timeval  ti, tf;
 
-  /* Calculate and draw points */
-  for(i=0; i < Y_RESN; i++)
-  {
-    for(j=0; j < X_RESN; j++)
-    {
-      z.real = z.imag = 0.0;
-      c.real = X_MIN + j * (X_MAX - X_MIN)/X_RESN;
-      c.imag = Y_MAX - i * (Y_MAX - Y_MIN)/Y_RESN;
-      k = 0;
+	if(rank == 0){	
+		/* Allocate result matrix of Y_RESN x X_RESN */
+		vres = (int *) malloc(Y_RESN * X_RESN * sizeof(int));
+		if (!vres){
+			fprintf(stderr, "Error allocating memory\n");
+			return 1;
+		}
+		for (i=0; i<Y_RESN; i++)
+			res[i] = vres + i*X_RESN;
+	}else{
+		vres = (int *) malloc(X_RESN * filasPorProceso * sizeof(int))
+		if (!vres){
+			fprintf(stderr, "Error allocating memory\n");
+			return 1;
+		}
+		for (i=0; i<filasPorProceso; i++)
+			res[i] = vres + i*X_RESN;
+	}
 
-      do
-      {    /* iterate for pixel color */
-        temp = z.real*z.real - z.imag*z.imag + c.real;
-        z.imag = 2.0*z.real*z.imag + c.imag;
-        z.real = temp;
-        lengthsq = z.real*z.real+z.imag*z.imag;
-        k++;
-      } while (lengthsq < 4.0 && k < maxIterations);
+	/* Start measuring time */
+	gettimeofday(&ti, NULL);
 
-      if (k >= maxIterations) res[i][j] = 0;
-      else res[i][j] = k;
-    }
-  }
+	filaInicio = rank*filasPorProceso;
+	filaFin = (rank+1)*filasPorProceso;
 
-  /* End measuring time */
-  gettimeofday(&tf, NULL);
-  fprintf (stderr, "(PERF) Time (seconds) = %lf\n", get_seconds(ti,tf));
+	/* Calculate and draw points */
+	for(i=filaInicio; i < filaFin; i++)
+	{
+		for(j=0; j < X_RESN; j++)
+		{
+			z.real = z.imag = 0.0;
+			c.real = X_MIN + j * (X_MAX - X_MIN)/X_RESN;
+			c.imag = Y_MAX - i * (Y_MAX - Y_MIN)/Y_RESN;
+			k = 0;
 
-  /* Print result out */
-  if( DEBUG ) {
-    for(i=0;i<Y_RESN;i++) {
-      for(j=0;j<X_RESN;j++)
-              printf("%3d ", res[i][j]);
-      printf("\n");
-    }
-  }
+			do
+			{    /* iterate for pixel color */
+				temp = z.real*z.real - z.imag*z.imag + c.real;
+				z.imag = 2.0*z.real*z.imag + c.imag;
+				z.real = temp;
+				lengthsq = z.real*z.real+z.imag*z.imag;
+				k++;
+			} while (lengthsq < 4.0 && k < maxIterations);
 
-  free(vres);
+			if (k >= maxIterations) res[i][j] = 0;
+			else res[i-rank*filasPorProceso][j] = k;
+		}
+	}
 
-  return 0;
+	/* End measuring time */
+	gettimeofday(&tf, NULL);
+	fprintf (stderr, "(PERF) Time (seconds) = %lf\n", get_seconds(ti,tf));
+
+	/* Print result out */
+	if( DEBUG ) {
+		for(i=0;i<Y_RESN;i++) {
+			for(j=0;j<X_RESN;j++)
+				printf("%3d ", res[i][j]);
+			printf("\n");
+		}
+	}
+
+	free(vres);
+	
+	MPI_Finalize();
+
+	return 0;
 }
+
